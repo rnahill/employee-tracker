5663a0799cbfdcbd8478f6e4379ddbd967362d6f
rnahill <rei333lly@gmail.com>
2023-12-17 16:33:55 -0500

    added values to seeds file and fixed syntax in both schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,27 @@
-DROP DATABASE IF EXISTS db_name
-CREATE DATABASE db_name
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
 
-USE db_name
+USE company_db;
 
-CREATE TABLE table_name (
-    id INT AUTO_INCREMENT PRIMARY KEY NOY NULL,
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(30)
 );
 
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,38 @@
-INSERT INTO table_name (name)
+-- Inserting values into the 'department' table
+INSERT INTO department (name)
 VALUES
-("value");
-("value");
+("Production"),
+("Editorial"),
+("Publicity"),
+("Marketing"),
+("Administration"),
+("Human Resources"),
+("Accounting");
+
+-- Inserting values into the 'role' table
+INSERT INTO role (title, salary, department_id)
+VALUES 
+("Typesetter", 50000, 1),
+("Copy Editor", 50000, 2),
+("Publicist", 60000, 3),
+("Marketing Manager", 90000, 4),
+("CEO", 150000, 5),
+("Human Resources Manager", 70000, 6),
+("Accountant", 100000, 7),
+("Accounting Manager", 120000, 8);
+
+-- Inserting values into the 'employee' table
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+("John", "Doe", 1, NULL),
+("Jane", "Doe", 2, NULL),
+("Jason", "Williams", 3, 4),
+("Shannon", "Marie", 4, NULL),
+("Grace", "Brown", 5, NULL),
+("Julia", "Crooks", 6, NULL),
+("Robert", "Jones", 7, 8),
+("Lily", "Heintz", 8, NULL);
+
+USE company_db;
+
+SHOW TABLES;
