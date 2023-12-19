210165b36140d3afe2f5ccb00d030c2c83821fc9
rnahill <rei333lly@gmail.com>
2023-12-19 15:58:23 -0500

    fixed syntax errors and fixed foreign key issues, removed uneeded comments

--- connection.js
+++ connection.js
@@ -1,15 +1,11 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-// Use environment variables to connect to database
 const db = mysql.createConnection(
     {
       host: 'localhost',
-      // MySQL user
-      user: process.env.user,
-      // MySQL password     
+      user: process.env.user,    
       password: process.env.password,
-      // MySQL databse 
       database: process.env.database
     },
     console.log(`Connected to the ${process.env.database} database.`)
--- db/seeds.sql
+++ db/seeds.sql
@@ -18,13 +18,17 @@ VALUES
 ("Human Resources", 70000, 5),
 ("Accountant", 100000, 6);
 
-
 -- Inserting values into the 'employee' table
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 ("Jane", "Doe", 1, 4),
 ("Shannon", "Willow", 2, 4),
 ("Grace", "Brown", 3, 4),
-("Julia", "Crooks", 4, NULL),
+("Julia", "Crooks", 4, 4),
 ("Robert", "Jones", 5, 4),
 ("Lily", "Heintz", 6, 4);
+
+-- Add FOREIGN KEY constraint
+ALTER TABLE employee
+ADD CONSTRAINT fk_manager
+FOREIGN KEY (manager_id) REFERENCES employee(id);
