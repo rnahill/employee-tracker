c9266cacb3e7ca24cfd323e96d5ccd2ca3d78817
rnahill <rei333lly@gmail.com>
2023-12-28 13:08:31 -0500

    refactored some code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-// const { table } = require('table');
+const { table } = require('table');
 
 const mysql = require('mysql2/promise');
 
@@ -9,77 +9,59 @@ const inquirer = require('inquirer');
 
 const init = async () => {
 
-  //   // use inquirer
+  const userPrompt = () => {
 
-  //   //use prepared_statement
+      inquirer.prompt([
 
-  //   const results = await db.query("SELECT * FROM table_name;");
-  
-  // // get data from results;
-  // const data = results[0];
-  // console.log(data);
+      {
+        type: "list",
+        name: "select",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments.",
+          "View all roles.",
+          "View all employees.",
+          "Add a department.",
+          "Add a role.",
+          "Add an employee.",
+          "Update an employee role."
+        ]
+      }
 
-  // // table module
-  // const arrOfArr = data.map( row => Object.values(row));
-  // // add column names
-  // arrOfArr.unshift(["id", "name"]);
-  // // print table
-  // console.log(table(arrOfArr));
+    ])
 
+    .then((answers) => {
+      const {select} = answers;
 
-const userPrompt = () => {
+      switch (select) {
+        case "View all departments.":
+          console.log("viewing departments");
+          break;
+        case "View all roles.":
+          console.log("viewing all roles");
+          break;
+        case "View all employees.":
+          console.log("viewing all employees");
+          break;
+        case "Add a department.":
+          console.log("adding a department");
+          break;
+        case "Add a role.":
+          console.log('adding a role');
+          break;
+        case "Add an employee.":
+          console.log('adding an employee');
+          break;
+        case "Update an employee role.":
+          console.log('updating an employee role');
+          break;
+      }
 
-    inquirer.prompt([
+    })
 
-    {
-       type: "list",
-       name: "select",
-       message: "What would you like to do?",
-       choices: [
-        "View all departments.",
-        "View all roles.",
-        "View all employees.",
-        "Add a department.",
-        "Add a role.",
-        "Add an employee.",
-        "Update an employee role."
-       ]
-    }
-
-])
-
-.then((answers) => {
-  const {select} = answers;
-
-  switch (select) {
-    case "View all departments.":
-      console.log("viewing departments");
-      break;
-    case "View all roles.":
-      console.log("viewing all roles");
-      break;
-    case "View all employees.":
-      console.log("viewing all employees");
-      break;
-    case "Add a department.":
-      console.log("adding a department");
-      break;
-    case "Add a role.":
-      console.log('adding a role');
-      break;
-    case "Add an employee.":
-      console.log('adding an employee');
-      break;
-    case "Update an employee role.":
-      console.log('updating an employee role');
-      break;
   }
 
-})
-
-}
-
-userPrompt();
+  userPrompt();
 
 }
 
